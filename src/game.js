c6b89651f20923765b2b09a4b97f530f30c0a9b5
kode-git <mariosessa64@gmail.com>
2022-07-18 17:21:44 +0200

    Adding fps

---
+++ src/game.js
@@ -0,0 +1,10 @@
+
+// getting WebGL context for the game canvas
+var game = document.getElementById('game')
+var gl = game.getContext('webgl')
+
+// getting 2D Context for the text canvas
+var text = document.getElementById('text')
+var c2d = text.getContext('2d')
+
+
